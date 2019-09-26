1ba507a2f7659aefe8e4f2b19152db7eda133688
Daniel Ade <Ldanielade@gmail.com>
2019-09-26 15:37:57 +1000

    adding contacts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Contact.create([{name:'bob',  address:'1234', city:'brisbane', state:'qld', phone:'23038203'}])
+Contact.create([{name:'bob',  address:'1234', city:'brisbane', state:'qld', phone:'23038203'}])
+Contact.create([{name:'bob',  address:'1234', city:'brisbane', state:'qld', phone:'23038203'}])
+Contact.create([{name:'bob',  address:'1234', city:'brisbane', state:'qld', phone:'23038203'}])
+Contact.create([{name:'bob',  address:'1234', city:'brisbane', state:'qld', phone:'23038203'}])
